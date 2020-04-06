b9aeb8b95eb447b9a96a56c61b860e888b6e9a10
susumoa <tobiasbori@gmail.com>
2020-04-06 14:29:37 +0100

    feat: add navigation to sumbit new deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -15,26 +15,6 @@ class NewDeck extends Component {
   submitDeckTitle = () => {
     const { value } = this.state
 
-    // const decks = getDecks().then(res => {
-    //   console.log('First result: ', res)
-    //   return res
-    // })
-
-    // Object.keys(getDecks().then(res => {
-    //   console.log('First result: ', res)
-    //   return res
-    // })).some(e => e === value)
-    //     ? Alert.alert(
-    //       'Error',
-    //       'Already existing deck',
-    //       [{text: 'OK', onPress: () => console.log('OK Pressed')},],
-    //       {cancelable: false},
-    //     )
-    //     : saveDeckTitle(value)
-
-
-    // this.props.navigation.navigate('New Deck')
-
    getDecks().then(res => {
      console.log('Decks before: ', res)
      Object.keys(res).some(e => e === value)
@@ -46,18 +26,9 @@ class NewDeck extends Component {
         )
       : saveDeckTitle(value)
         this.setState({value: ''})
+        this.props.navigation.navigate('Deck')
      console.log('----------------------------------')
     })
-      
-    
-
-    // AsyncStorage.saveDeckTitle
-    //check if existing title
-
-    // route to the new deck
-    // this.props.navigation.navigate('New Deck')
-
-    
   }
 
   render() {
